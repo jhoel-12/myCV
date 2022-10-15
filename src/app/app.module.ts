8553a98d68c5d4f96171d572978963bd8db28071
desarrollador198 <desarrollador198_us@minedu.gob.pe>
2022-10-15 18:59:40 -0500

    nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AbautComponent } from './components/abaut/abaut.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { FooterComponent } from './components/home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AbautComponent,
+    SkillComponent,
+    ContactComponent,
+    BlogComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
