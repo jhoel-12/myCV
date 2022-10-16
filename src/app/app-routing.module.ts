dc3cecaa579f5b2ea1dbd8efd37d23367c171045
desarrollador198 <desarrollador198_us@minedu.gob.pe>
2022-10-15 21:40:05 -0500

    cambios de formulario de contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbautComponent } from './components/abaut/abaut.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { SkillComponent } from './components/skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch :'full' },
+  {path: 'home', component:HomeComponent},
+  {path: 'abaut', component:AbautComponent},
+  {path: 'blog', component:BlogComponent},
+  {path: 'contact', component:ContactComponent},
+  {path:'skill', component:SkillComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
